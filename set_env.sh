b18f1d6e3ca3c82267f7c6370c8e61ed832596ab
Mikaël De Lachevrotière <mikdelac@pop-os.home>
2025-01-18 15:08:04 -0500

    added set_env script and strategies

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if .env file exists
+if [ ! -f .env ]; then
+  echo ".env file not found!"
+  exit 1
+fi
+
+# Export variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+echo "Environment variables set from .env file."
